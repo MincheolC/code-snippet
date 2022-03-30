2f73feba18980a58b62f51e7361ce166eaa8d8de
MincheolCha <mc.cha@greenlabs.co.kr>
2022-03-30 23:48:43 +0900

    init

---
+++ tinyUrl/index.js
@@ -0,0 +1,11 @@
+// 단축 url을 제공해야 함
+// - 매일 1억 개의 단축 URL 생성
+// - 1160 req/sec
+// - URL avg length = 100
+
+const sampleUrl = "https://jamboard.google.com/d/1mGQzQ4JzHBzR1VkFSUGzSYe4v3E5CZPI2-WRZoYYi24/viewer"
+
+// 해시 후 충돌 해소
+
+
+// base-62 변환
